2c38b43d91d5b9c985ba199c8651e1097c5c7c0d
Camila Arredondo <cami.arredondo95@gmail.com>
2023-11-14 11:44:36 -0300

    init 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { AdminPAC } from './Page/Home';
 
 function App() {
+  //use efect que se actualice cada 2 segundos
+  
   return (
-<AdminPAC />
-);
+   <AdminPAC></AdminPAC>
+  );
 }
 
 export default App;
