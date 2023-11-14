3cef8056ff7aedf5d5ab0944a1275ec462ad0e93
Camila Arredondo <cami.arredondo95@gmail.com>
2023-11-14 11:43:06 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { AdminPAC } from './Page/Home';
+
+function App() {
+  return (
+<AdminPAC />
+);
+}
+
+export default App;
---
+++ src/Page/Home.tsx
@@ -0,0 +1,7 @@
+export function AdminPAC() {
+return(
+    <div>
+        <h1>PAC</h1>
+    </div>
+)
+}
